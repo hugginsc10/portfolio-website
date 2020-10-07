d28ff8b20893ea6e519f760fa4ade07b4bd85ac2
chas <hugginsc10@gmail.com>
2020-10-07 14:03:15 -0400

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
-
   constructor(props){
     super(props);
     this.state = {
