540532f28453194d56f491950693edc5ecaaf4bc
chas <hugginsc10@gmail.com>
2020-11-02 19:49:10 -0500

    updated bio

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -89,28 +89,6 @@ class Contact extends Component {
                <a href="mailto:hugginsc10@gmail.com" alt="">Hugginsc10@gmail.com</a>
 					   </p>
 				   </div>
-
-               {/* <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div> */}
             </aside>
       </div>
    </section>
